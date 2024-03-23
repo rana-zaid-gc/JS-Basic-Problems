c7f0815f877fd1b080fd8f8a83a265a98595fed0
rana zaid <itszaidrajpoot@gmail.com>
2024-03-24 03:44:15 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,76 @@
+// 1-Calculate the Square of a Number.
+
+
+// function squareNum(){
+//     let num=parseInt(prompt("Enter a number"));
+//     let sqNum=num*num;
+//     console.log(sqNum);
+// }
+// squareNum()
+
+
+
+// 2-Calculate Square-root of a Number.
+
+
+// function sqrtNum(){
+//     let number=parseInt(prompt("Enter a number"));
+//     let sqNum=Math.sqrt(number);
+//    console.log(sqNum);
+// }
+// sqrtNum()
+
+// 3-copy-all-elements-from-an-array-to-another-array.
+
+
+// let array=[1,2,3,4,5,6,7];
+// let array2=array.slice()
+// console.log(array2)
+
+
+// 4-maximum-and-minimum-between-two-numbers-using-functions, if you have not been proficient so you can do it simply in your own way.
+
+// function maximNum(){
+//     let num1=Number(prompt("enter a number"));
+//     let num2=Number(prompt("enter a number"));
+//     if(num1>num2){
+//         console.log("First number is greater")
+//     }
+//     else if(num1==num2){
+//         console.log("Both are equal");
+//     }
+//     else{
+//         console.log("Second number is greater")
+//     }
+// }
+// maximNum()
+
+
+
+// 5-count-total-number-of-negative-elements-in-an-array
+
+// function negativeArray(){
+//     let array=[2,3,-1,5,-10];
+// var array2=[]
+// for(let i=0; i<=array.length;i++){
+//     if(array[i]<array2){
+//        console.log(array[i]) ;
+//     }
+// }
+// }
+// negativeArray()
+
+
+
+// 6-sum of all even numbers between 1 to n, n is taken from the user. 
+
+// let number=Number(prompt("Enter a number"))
+// var sum=0;
+// for(i=1;i<=number;i++){
+//     if(i%2==0){
+//         sum=sum+i;
+//         console.log(sum)
+//     }
+// }
+
+
